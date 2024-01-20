d7b4f8c7e995e004c62167cd5864b82bd9148502
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2024-01-20 22:28:31 +0700

    fix: gradient

---
+++ src/layouts/Circle.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let size: number;
+    export let top: number | null = null;
+    export let right: number | null = null;
+    export let bottom: number | null = null;
+    export let left: number | null = null;
+
+    $: style = `
+        width: ${size}px;
+        height: ${size}px;
+        top: ${top ? `calc(${top} - ${size / 2}px)` : "auto"};
+        right: ${right ? `calc(${right} - ${size / 2}px)` : "auto"};
+        bottom: ${bottom ? `calc(${bottom} - ${size / 2}px)` : "auto"};
+        left: ${left ? `calc(${left} - ${size / 2}px)` : "auto"};
+    `;
+</script>
+
+<div class={`${$$props.class} absolute -z-10 mix-blend-multiply`} {style}></div>
